57cbf3dfcff69b272272db1d3be0883e81763b93
PIETRO <prodrigano.ieu2022@student.ie.edu>
2024-11-18 13:34:22 +0100

    please work

---
+++ launch.sh
@@ -0,0 +1,21 @@
+export CYCLES_PORT=50010
+
+cat<<EOF> config.yaml
+gameHeight: 500
+gameWidth: 500
+gameBannerHeight: 100
+gridHeight: 100
+gridWidth: 100
+maxClients: 60
+enablePostProcessing: false
+EOF
+
+./build/bin/server &
+sleep 1
+
+for i in {1..4}
+do
+./build/bin/client randomio$i &
+
+done
+./build/bin/client_Pietro pietro &
---
+++ src/client/client_Pietro.cpp
@@ -0,0 +1,48 @@
+#include "api.h"
+#include "utils.h"
+#include <string>
+#include <iostream>
+
+using namespace cycles;
+
+class BotClient {
+  Connection connection;
+  std::string name;
+  GameState state;
+
+  void sendMove() {
+    connection.sendMove(Direction::north);
+  }
+
+  void receiveGameState() {
+    state = connection.receiveGameState();
+    std::cout<<"There are "<<state.players.size()<<" players"<<std::endl;
+  }
+
+public:
+  BotClient(const std::string &botName) : name(botName) {
+    connection.connect(name);
+    if (!connection.isActive()) {
+      exit(1);
+    }
+  }
+
+  void run() {
+    while (connection.isActive()) {
+      receiveGameState();
+      sendMove();
+    }
+  }
+
+};
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <bot_name>" << std::endl;
+    return 1;
+  }
+  std::string botName = argv[1];
+  BotClient bot(botName);
+  bot.run();
+  return 0;
+}
